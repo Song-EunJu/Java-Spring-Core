e72761c8610a16deb5528e7cf6b1f5b04229a665
Song-EunJu <jd06280@naver.com>
2021-07-28 00:16:17 +0900

    [ADD] 스프링 빈 조회;

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -4,7 +4,6 @@ import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class ApplicationContextInfoTest {
@@ -27,16 +26,10 @@ class ApplicationContextInfoTest {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-            // BeanDefinition - 빈 하나하나에 대한 메타데이터 정보
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
-// 스프링 내부에서 무언가 작업을 하려고 등록한 빈들이 아니라 내가 애플리케이션을 등록하기 위해 등록한 빈
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name = "+beanDefinitionName+" object = "+bean);
             }
-            // getRole()
-
         }
     }
 }
-
-// 스프링 내부적으로 스프링 자체를 확장하기 위해 쓰는 bean들
